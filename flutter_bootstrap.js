9edb15c4fb16e36f4c21f1c7c7f39851038314bc
muktabd <mukta192426@gmail.com>
2025-05-10 13:22:14 +0800

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1006264506"
+    serviceWorkerVersion: "3990450287"
   }
 });
