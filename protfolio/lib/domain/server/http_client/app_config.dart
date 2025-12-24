1d260bf5d2195ca23c4b2a4f5770769f16b103f3
ABDULLAH IBNA MUKTA <56233279+muktabd@users.noreply.github.com>
2025-12-24 15:18:51 +0800

    will publish new version by fixing cors issue

--- protfolio/lib/domain/server/http_client/app_config.dart
+++ protfolio/lib/domain/server/http_client/app_config.dart
@@ -1,7 +1,7 @@
 class AppConfig {
   // static String baseUrlControllerUrl = 'http://private.rbftechzone.com/api/v1/client/projects/4';
   // static String defaultBaseUrl = "http://mukta.rbfgroupbd.com";
-  static String baseUrl = 'http://practicingmuslim.org';
+  static String baseUrl = 'https://practicingmuslim.org';
   static String contactMeUri = "/muktabd/api/contact_me";
   static String contactMeRequestUri = "/muktabd/api/contact_me/request";
 
